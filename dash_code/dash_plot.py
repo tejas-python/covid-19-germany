120adef3829f6322db861c97ecc1d42c79c98873
tejas-python <tejasa2207@outlook.com>
2021-04-07 13:21:03 +0530

    new graph in dash

--- dash_code/dash_plot.py
+++ dash_code/dash_plot.py
@@ -14,6 +14,7 @@ df_grouped2= pd.read_csv('./dash_code/grouped2.csv')
 df_germany1= pd.read_csv('./dash_code/g1.csv',index_col='Unnamed: 0')
 result= pd.read_csv('./dash_code/result.csv')
 result_reg= pd.read_csv('./dash_code/result_reg')
+df_germany = df_germany.drop(columns=['stringency_index'])
 def multi_plot_scatter(df, title, addAll = True):
     fig = go.Figure()
     
@@ -149,8 +150,8 @@ def multi_plot(df, title, addAll = True):
     return fig
 def multi_distplot(df, title, addAll = True):
     fig = go.Figure()
-    df = df.set_index('date')
-    df = df.drop(columns=['month', 'year'],axis=1)
+    
+    df = df.drop(columns=['month', 'year','date'],axis=1)
     for column in df.columns.to_list():
          fig.add_trace(
         go.Histogram(x=df[column],name=column, )
@@ -212,6 +213,8 @@ app.layout = html.Div(children =[html.H1(children='Covid -19 data analysis for g
                             html.Div(children='Model development using SIR and Multiple regression'),
                             dcc.Graph(id='dash_graph2',
                                      figure = fig2),
+                            dcc.Graph(id='dash_graph7',
+                                     figure = multi_distplot(df_germany, "Histogram plot For the variables")),
                             dcc.Graph(id='dash_graph3',
                                      figure = fig1)
                                  
